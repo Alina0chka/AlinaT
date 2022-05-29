0842ec5bafaf782b023c27343f73dc9b92d9c918
Alina0chka <so_lin@bk.ru>
2022-05-29 09:54:15 +0300

    Delnull

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -26,33 +26,39 @@ namespace Calculator
 
                 Console.WriteLine("Введите второе число:");
                 b = Convert.ToDouble(Console.ReadLine());
-
-                if (oper == '+')
+                if (b == 0 & oper == '/')
                 {
-                    total = a + b;
-                    Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
-                }
-
-                else if (oper == '-')
-                {
-                    total = a - b;
-                    Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
-                }
-
-                else if (oper == '*')
-                {
-                    total = a * b;
-                    Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
-                }
-
-                else if (oper == '/')
-                {
-                    total = a / b;
-                    Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                    Console.WriteLine("Деление на 0 запрещено!");
                 }
                 else
                 {
-                    Console.WriteLine("Неизвестный оператор.");
+                    if (oper == '+')
+                    {
+                        total = a + b;
+                        Console.WriteLine("Cумма " + a + " и " + b + " равна " + total + ".");
+                    }
+
+                    else if (oper == '-')
+                    {
+                        total = a - b;
+                        Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+                    }
+
+                    else if (oper == '*')
+                    {
+                        total = a * b;
+                        Console.WriteLine("Умножение " + a + " на " + b + " равно " + total + ".");
+                    }
+
+                    else if (oper == '/')
+                    {
+                        total = a / b;
+                        Console.WriteLine("Деление " + a + " на " + b + " равно " + total + ".");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Неизвестный оператор.");
+                    }
                 }
                 Console.WriteLine("Вы хотите продолжить работу с калькулятором? (д/н)");
                 again = Convert.ToChar(Console.ReadLine());
